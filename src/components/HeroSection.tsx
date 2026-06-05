28bcdb089a2b31a6cf7dfb4f0012156869822074
yoonji270088 <yoonji270088@gmail.com>
2026-06-05 15:28:31 +0900

    remove height

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -40,7 +40,7 @@ export default function HeroSection({ phase }: Props) {
     <div
       style={{
         width: "100%",
-        height: "720px",
+        // height: "720px",
         position: "relative",
         overflow: "hidden",
         flexShrink: 0,
